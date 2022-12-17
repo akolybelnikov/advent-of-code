c4cdadcbe61303c1d9e5597517ce6012fe6eee41
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2022-12-17 19:17:47 +0100

    day 4

--- day_4/cleanup_test.go
+++ day_4/cleanup_test.go
@@ -1,15 +1,16 @@
-package advent_test
+package day_4_test
 
 import (
 	a "github.com/akolybelnikov/advent-of-code"
+	c "github.com/akolybelnikov/advent-of-code/day_4"
 	"github.com/go-playground/assert/v2"
 	"testing"
 )
 
 func TestFindContainedPairs(t *testing.T) {
 	t.Run("should handle the default input", func(t *testing.T) {
-		data, _ := a.ReadDataBytes("testdata/cleanup/input.txt")
-		total, err := a.FindContainedPairs(data)
+		data, _ := a.ReadDataBytes("testdata/input.txt")
+		total, err := c.FindContainedPairs(data)
 
 		if err != nil {
 			t.Log(err)
@@ -22,8 +23,8 @@ func TestFindContainedPairs(t *testing.T) {
 
 func TestFindOverlappingPairs(t *testing.T) {
 	t.Run("should handle the default input", func(t *testing.T) {
-		data, _ := a.ReadDataBytes("testdata/cleanup/input.txt")
-		total, err := a.FindOverlappingPairs(data)
+		data, _ := a.ReadDataBytes("testdata/input.txt")
+		total, err := c.FindOverlappingPairs(data)
 
 		if err != nil {
 			t.Log(err)
