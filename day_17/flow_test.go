d50307b70b69c8f1a65f38da5896d1f6845e3d45
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2022-12-23 12:10:08 +0100

    day 17 part 2

--- day_17/flow_test.go
+++ day_17/flow_test.go
@@ -19,12 +19,32 @@ func TestFlow(t *testing.T) {
 		fmt.Println(time.Since(start))
 	})
 
-	t.Run("test input", func(t *testing.T) {
+	t.Run("test input part 2", func(t *testing.T) {
+		start := time.Now()
+		data, _ := utils.ReadDataBytes("testdata/input_test.txt")
+		pattern := data[:len(data)-1]
+		t.Log(len(pattern))
+		top := day_17.Run2(&pattern, 1000000000000)
+		t.Log(top)
+		fmt.Println(time.Since(start))
+	})
+
+	t.Run("long input", func(t *testing.T) {
 		start := time.Now()
 		data, _ := utils.ReadDataBytes("testdata/input.txt")
 		pattern := data[:len(data)-1]
 		t.Log(len(pattern))
 		top := day_17.Run(&pattern, 2022)
+		t.Log(top)
+		fmt.Println(time.Since(start))
+	})
+
+	t.Run("long input part 2", func(t *testing.T) {
+		start := time.Now()
+		data, _ := utils.ReadDataBytes("testdata/input.txt")
+		pattern := data[:len(data)-1]
+		t.Log(len(pattern))
+		top := day_17.Run2(&pattern, 1000000000000)
 		t.Log(top)
 		fmt.Println(time.Since(start))
 	})
