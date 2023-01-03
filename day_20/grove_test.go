51e195bf957b6a1033a8e72c858f1f286bc776ec
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-01-03 09:51:05 +0100

    day 20 part 2

--- day_20/grove_test.go
+++ day_20/grove_test.go
@@ -33,3 +33,21 @@ func TestGrovePositioning(t *testing.T) {
 		assert.Equal(t, grove, 14888)
 	})
 }
+
+func TestGrovePositioning2(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		start := time.Now()
+		dataArr, _ := utils.MakeBytesArray(&testInput)
+		grove := day_20.GrovePositioning2(dataArr)
+		fmt.Println(time.Since(start))
+		assert.Equal(t, grove, 1623178306)
+	})
+
+	t.Run("input", func(t *testing.T) {
+		start := time.Now()
+		dataArr, _ := utils.MakeBytesArray(&input)
+		grove := day_20.GrovePositioning2(dataArr)
+		fmt.Println(time.Since(start))
+		assert.Equal(t, grove, 3760092545849)
+	})
+}
