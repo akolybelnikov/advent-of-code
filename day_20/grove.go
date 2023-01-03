63dbe37a7b57cfa63f1dae94b2b33c7e34272cae
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-01-03 09:51:05 +0100

    day 20 part 2

--- day_20/grove.go
+++ day_20/grove.go
@@ -8,6 +8,8 @@ type node struct {
 	prev *node
 }
 
+const dk = 811589153
+
 func GrovePositioning(arr *[]*[]byte) int {
 	var idx0 int
 	mix := make(list)
@@ -20,40 +22,64 @@ func GrovePositioning(arr *[]*[]byte) int {
 	}
 
 	mix.link(&nums)
-	m := len(nums) - 1
+	mix.move(len(nums))
 
-	for nodeIdx, _ := range nums {
-		cur := mix[nodeIdx]
-		temp := cur
-		if cur.val == 0 {
-			continue
+	return mix.coordinates(idx0)
+}
+
+func GrovePositioning2(arr *[]*[]byte) int {
+	var idx0 int
+	mix := make(list)
+	nums := encryptedFile(arr)
+	for i, num := range nums {
+		if num == 0 {
+			idx0 = i
 		}
-		if cur.val > 0 {
-			for i := 0; i < cur.val%m; i++ {
-				temp = temp.next
-			}
-		} else {
-			for i := 0; i >= cur.val%m; i-- {
-				temp = temp.prev
-			}
-		}
-		if temp == cur {
-			continue
-		}
-		update(cur, temp)
+		mix[i] = &node{val: num * dk}
+	}
+
+	mix.link(&nums)
+	for i := 0; i < 10; i++ {
+		mix.move(len(nums))
 	}
 
 	return mix.coordinates(idx0)
 }
 
 func (m *list) link(nums *[]int) {
-	for i, _ := range *nums {
+	for i := range *nums {
 		(*m)[i].next = (*m)[(i+1)%len(*m)]
 		(*m)[i].prev = (*m)[(i-1+len(*m))%len(*m)]
 	}
 }
 
-func update(n *node, t *node) {
+func (m *list) move(l int) {
+	n := l - 1
+
+	for nodeIdx := 0; nodeIdx < l; nodeIdx++ {
+		cur := (*m)[nodeIdx]
+		temp := cur
+		if cur.val == 0 {
+			continue
+		}
+		if cur.val > 0 {
+			for i := 0; i < cur.val%n; i++ {
+				temp = temp.next
+			}
+		} else {
+			for i := 0; i >= cur.val%n; i-- {
+				temp = temp.prev
+			}
+		}
+		if temp == cur {
+			continue
+		}
+
+		cur.update(temp)
+	}
+}
+
+func (n *node) update(t *node) {
 	n.next.prev = n.prev
 	n.prev.next = n.next
 	n.next = t.next
