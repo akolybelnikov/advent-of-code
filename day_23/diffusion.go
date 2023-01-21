b6466443a20b18fe1d38df0212aea27ca3963799
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-01-21 15:54:27 +0100

    parallel solution with memory analysis

--- day_23/diffusion.go
+++ day_23/diffusion.go
@@ -3,6 +3,7 @@ package day_23
 import (
 	"github.com/mitchellh/hashstructure/v2"
 	"github.com/puzpuzpuz/xsync"
+	"sync"
 )
 
 const (
@@ -26,6 +27,7 @@ type State uint8
 type Cell struct {
 	State    State
 	Proposed xsync.Counter
+	*Point
 }
 
 type Point struct {
@@ -34,7 +36,7 @@ type Point struct {
 }
 
 type Grid struct {
-	Cells xsync.MapOf[Point, *Cell]
+	Cells sync.Map
 	cur   int
 	rules map[int][]int
 }
@@ -73,13 +75,14 @@ func UnstableDiffusion2(arr *[]*[]byte) int {
 	return rounds
 }
 
-func NewCell() *Cell {
-	return &Cell{}
+func NewCell(p *Point) *Cell {
+	return &Cell{
+		Point: p,
+	}
 }
 
 func NewGrid(arr *[]*[]byte) *Grid {
 	g := &Grid{
-		Cells: *xsync.NewTypedMapOf[Point, *Cell](getHash),
 		rules: map[int][]int{
 			N: {N, NW, NE},
 			S: {S, SW, SE},
@@ -93,39 +96,49 @@ func NewGrid(arr *[]*[]byte) *Grid {
 	return g
 }
 
-func getHash(p Point) uint64 {
+func GetHash(p Point) uint64 {
 	hash, _ := hashstructure.Hash(p, hashstructure.FormatV2, nil)
 	return hash
 }
 
 func (g *Grid) makeCells(arr *[]*[]byte) {
+	var wg sync.WaitGroup
+
 	for i, v := range *arr {
 		for i2, b := range *v {
-			p := Point{X: int32(i2), Y: int32(i)}
-			c := NewCell()
-			if b == '#' {
-				c.State = ELF
-			}
-			g.Cells.Store(p, c)
+			wg.Add(1)
+			go func(idx, idx2 int, bt byte) {
+				defer wg.Done()
+				p := Point{X: int32(idx2), Y: int32(idx)}
+				c := NewCell(&p)
+				if bt == '#' {
+					c.State = ELF
+				}
+				h := GetHash(p)
+				g.Cells.Store(h, c)
+			}(i, i2, b)
 		}
 	}
+
+	wg.Wait()
 }
 
 func (g *Grid) getMinRectangular() [2]Point {
 	var left, right, top, bottom int32
-	g.Cells.Range(func(p Point, c *Cell) bool {
+	g.Cells.Range(func(k, v any) bool {
+		c := v.(*Cell)
 		if c.State == ELF {
-			if p.X < left {
-				left = p.X
+			if c.X < left {
+				left = c.X
 			}
-			if p.X > right {
-				right = p.X
+			if c.X > right {
+				right = c.X
 			}
-			if p.Y < top {
-				top = p.Y
+			if c.Y < top {
+				top = c.Y
 			}
-			if p.Y > bottom {
-				bottom = p.Y
+			if c.Y > bottom {
+				bottom = c.Y
 			}
 		}
 		return true
@@ -143,20 +156,27 @@ func (g *Grid) getEmptyCellCount() int {
 	for i := rect[0].Y; i <= rect[1].Y; i++ {
 		for j := rect[0].X; j <= rect[1].X; j++ {
 			p := Point{X: j, Y: i}
-			if c, ok := g.Cells.Load(p); ok && c.State == EMPTY {
-				count++
-			} else if !ok {
+			h := GetHash(p)
+			if v, ok := g.Cells.Load(h); ok {
+				c := v.(*Cell)
+				if c.State == EMPTY {
+					count++
+				}
+			} else {
 				count++
 			}
 		}
 	}
+
 	return count
 }
 
 func (g *Grid) hasNeighborElves(ps *[8]Point) byte {
 	var bt byte
 	for i, p := range *ps {
-		if c, ok := g.Cells.Load(p); ok {
+		h := GetHash(p)
+		if v, ok := g.Cells.Load(h); ok {
+			c := v.(*Cell)
 			if c.State == ELF {
 				bt |= byte(1 << i)
 			}
@@ -180,24 +200,20 @@ func (p *Point) getNeighborCells() [8]Point {
 	}
 }
 
-func (g *Grid) proposeOne(p *Point) *[2]Point {
+func (g *Grid) proposeOne(p Point) *[2]Point {
 	nc := p.getNeighborCells()
 	hn := g.hasNeighborElves(&nc)
-	if hn == 0 {
-		return nil
-	} else {
+	if hn > 0 {
 		d := g.cur
 		for i := 0; i < 4; i++ {
 			rules := g.rules[d]
 			if hn&byte(1<<rules[0]) == 0 && hn&byte(1<<rules[1]) == 0 && hn&byte(1<<rules[2]) == 0 {
-				cell := NewCell()
-				if c, ok := g.Cells.LoadOrStore(nc[d], cell); ok {
-					cell.Proposed.Add(c.Proposed.Value())
-				} else {
-					cell.Proposed.Inc()
-				}
-
-				return &[2]Point{*p, nc[d]}
+				cell := NewCell(&nc[d])
+				h := GetHash(nc[d])
+				v, _ := g.Cells.LoadOrStore(h, cell)
+				c := v.(*Cell)
+				c.Proposed.Inc()
+				return &[2]Point{p, nc[d]}
 			}
 			d = (d + 1) % 4
 		}
@@ -207,55 +223,67 @@ func (g *Grid) proposeOne(p *Point) *[2]Point {
 }
 
 func (g *Grid) Propose(iteration int) []*[2]Point {
-	canMove := make([]*[2]Point, 0)
+	proposed := make(chan *[2]Point)
+	wg := sync.WaitGroup{}
+
 	g.cur = iteration % 4
-	g.Cells.Range(func(p Point, c *Cell) bool {
+	g.Cells.Range(func(k, v any) bool {
+		c := v.(*Cell)
 		if c.State == ELF {
-			nc := p.getNeighborCells()
-			hn := g.hasNeighborElves(&nc)
-			if hn > 0 {
-				d := g.cur
-				for i := 0; i < 4; i++ {
-					rules := g.rules[d]
-					if hn&byte(1<<rules[0]) == 0 && hn&byte(1<<rules[1]) == 0 && hn&byte(1<<rules[2]) == 0 {
-						if cell, ok := g.Cells.Load(nc[d]); ok {
-							cell.Proposed.Inc()
-						} else {
-							cl := NewCell()
-							cl.Proposed.Inc()
-							g.Cells.Store(nc[d], cl)
-						}
-						canMove = append(canMove, &[2]Point{p, nc[d]})
-						return true
-					}
-					d = (d + 1) % 4
+			wg.Add(1)
+			go func(p *Point) {
+				defer wg.Done()
+				move := g.proposeOne(*p)
+				if move != nil {
+					proposed <- move
 				}
-			}
+			}(c.Point)
 		}
 		return true
 	})
 
-	return canMove
+	go func() {
+		wg.Wait()
+		close(proposed)
+	}()
+
+	elvesCanMove := make([]*[2]Point, 0)
+	for move := range proposed {
+		elvesCanMove = append(elvesCanMove, move)
+	}
+
+	return elvesCanMove
 }
 
 func (g *Grid) moveOne(move *[2]Point) {
-	if c, ok := g.Cells.Load(move[1]); ok {
+	h1 := GetHash(move[1])
+	if v, ok := g.Cells.Load(h1); ok {
+		c := v.(*Cell)
 		if c.Proposed.Value() == 1 {
 			c.State = ELF
-			e, _ := g.Cells.Load(move[0])
+			h0 := GetHash(move[0])
+			ve, _ := g.Cells.Load(h0)
+			e := ve.(*Cell)
 			e.State = EMPTY
 		}
 	}
 }
 
 func (g *Grid) Move(moves []*[2]Point) {
+	wg := sync.WaitGroup{}
 	for _, move := range moves {
-		if c, ok := g.Cells.Load(move[1]); ok {
-			if c.Proposed.Value() == 1 {
-				c.State = ELF
-				e, _ := g.Cells.Load(move[0])
-				e.State = EMPTY
-			}
+		wg.Add(1)
+		go func(m *[2]Point) {
+			defer wg.Done()
+			g.moveOne(m)
+		}(move)
+	}
+	wg.Wait()
+
+	for _, move := range moves {
+		h := GetHash(move[1])
+		if v, ok := g.Cells.Load(h); ok {
+			c := v.(*Cell)
 			c.Proposed.Reset()
 		}
 	}
