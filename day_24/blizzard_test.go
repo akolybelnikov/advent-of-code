85a03af317c55824f7c0fedbbdb5351d8ad6d73f
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-01-26 10:24:15 +0100

    day 24 part 1

---
+++ day_24/blizzard_test.go
@@ -0,0 +1,35 @@
+package day_24_test
+
+import (
+	_ "embed"
+	utils "github.com/akolybelnikov/advent-of-code"
+	"github.com/akolybelnikov/advent-of-code/day_24"
+	"github.com/go-playground/assert/v2"
+	"testing"
+)
+
+//go:embed testdata/input_test.txt
+var inputTest []byte
+
+//go:embed testdata/input.txt
+var input []byte
+
+func TestMakeBlizzards(t *testing.T) {
+	arr, _ := utils.MakeBytesArray(&inputTest)
+	blizzards := day_24.MakeBlizzards(arr)
+	assert.Equal(t, 19, len(*blizzards))
+}
+
+func TestFindPath(t *testing.T) {
+	t.Run("test input", func(t *testing.T) {
+		arr, _ := utils.MakeBytesArray(&inputTest)
+		minutes := day_24.FindPath(arr)
+		t.Logf("minutes: %d", minutes)
+	})
+
+	t.Run("input", func(t *testing.T) {
+		arr, _ := utils.MakeBytesArray(&input)
+		minutes := day_24.FindPath(arr)
+		t.Logf("minutes: %d", minutes)
+	})
+}
