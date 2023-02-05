b439fd92e4cf678c313936bb84f9e8c90e8e4a9c
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-02-05 11:19:06 +0100

    day 25

---
+++ day_25/snafu.go
@@ -0,0 +1,54 @@
+package day_25
+
+import (
+	"fmt"
+)
+
+func ConvertToDecimalSum(input *[]*[]byte) (sum int) {
+	for _, v := range *input {
+		sum += ConvertToDecimal(v)
+	}
+
+	return
+}
+
+func ConvertToSnafu(sum int) string {
+	var result string
+
+	for sum > 0 {
+		rem := sum % 5
+		sum /= 5
+		switch {
+		case rem <= 2:
+			result = fmt.Sprintf("%d", rem) + result
+		case rem == 3:
+			result = "=" + result
+			sum++
+		case rem == 4:
+			result = "-" + result
+			sum++
+		}
+	}
+
+	return result
+}
+
+func ConvertToDecimal(input *[]byte) int {
+	var sum int
+	coef := 1
+	for i := len(*input) - 2; i >= 0; i-- {
+		switch (*input)[i] {
+		case '=':
+			sum -= 2 * coef
+		case '-':
+			sum -= coef
+		case '1':
+			sum += coef
+		case '2':
+			sum += 2 * coef
+		}
+		coef *= 5
+	}
+
+	return sum
+}
---
+++ day_25/snafu_test.go
@@ -0,0 +1,203 @@
+package day_25_test
+
+import (
+	_ "embed"
+	utils "github.com/akolybelnikov/advent-of-code"
+	"github.com/akolybelnikov/advent-of-code/day_25"
+	"github.com/go-playground/assert/v2"
+	"testing"
+)
+
+//go:embed testdata/test_input.txt
+var testInput []byte
+
+//go:embed testdata/input.txt
+var input []byte
+
+func TestConvertDecimalSumToSnafu(t *testing.T) {
+	t.Run("test input", func(t *testing.T) {
+		arr, _ := utils.MakeBytesArray(&testInput)
+		sum := day_25.ConvertToDecimalSum(arr)
+		assert.Equal(t, 4890, sum)
+		assert.Equal(t, "2=-1=0", day_25.ConvertToSnafu(sum))
+	})
+
+	t.Run("real input", func(t *testing.T) {
+		arr, _ := utils.MakeBytesArray(&input)
+		sum := day_25.ConvertToDecimalSum(arr)
+		assert.Equal(t, 6593948721415, sum)
+		t.Logf("SNAFU: %s", day_25.ConvertToSnafu(sum))
+	})
+}
+
+func TestConvertToDecimal(t *testing.T) {
+	tc := []struct {
+		name  string
+		snafu []byte
+		want  int
+	}{
+		{
+			name:  "test 1=-0-2",
+			snafu: []byte("1=-0-2\n"),
+			want:  1747,
+		},
+		{
+			name:  "test 12111",
+			snafu: []byte("12111\n"),
+			want:  906,
+		},
+		{
+			name:  "test 2=0=",
+			snafu: []byte("2=0=\n"),
+			want:  198,
+		},
+		{
+			name:  "test 21",
+			snafu: []byte("21\n"),
+			want:  11,
+		},
+		{
+			name:  "test 2=01",
+			snafu: []byte("2=01\n"),
+			want:  201,
+		},
+		{
+			name:  "test 111",
+			snafu: []byte("111\n"),
+			want:  31,
+		},
+		{
+			name:  "test 20012",
+			snafu: []byte("20012\n"),
+			want:  1257,
+		},
+		{
+			name:  "test 112",
+			snafu: []byte("112\n"),
+			want:  32,
+		},
+		{
+			name:  "test 1=-1=",
+			snafu: []byte("1=-1=\n"),
+			want:  353,
+		},
+		{
+			name:  "test 1-12",
+			snafu: []byte("1-12\n"),
+			want:  107,
+		},
+		{
+			name:  "test 12",
+			snafu: []byte("12\n"),
+			want:  7,
+		},
+		{
+			name:  "test 1=",
+			snafu: []byte("1=\n"),
+			want:  3,
+		},
+		{
+			name:  "test 122",
+			snafu: []byte("122\n"),
+			want:  37,
+		},
+	}
+	for _, c := range tc {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			got := day_25.ConvertToDecimal(&c.snafu)
+			assert.Equal(t, c.want, got)
+		})
+	}
+}
+
+func TestConvertToSnafu(t *testing.T) {
+	tc := []struct {
+		name string
+		sum  int
+		want string
+	}{
+		{
+			name: "test 1",
+			sum:  1,
+			want: "1",
+		},
+		{
+			name: "test 2",
+			sum:  2,
+			want: "2",
+		},
+		{
+			name: "test 3",
+			sum:  3,
+			want: "1=",
+		},
+		{
+			name: "test 4",
+			sum:  4,
+			want: "1-",
+		},
+		{
+			name: "test 5",
+			sum:  5,
+			want: "10",
+		},
+		{
+			name: "test 6",
+			sum:  6,
+			want: "11",
+		},
+		{
+			name: "test 7",
+			sum:  7,
+			want: "12",
+		},
+		{
+			name: "test 8",
+			sum:  8,
+			want: "2=",
+		},
+		{
+			name: "test 9",
+			sum:  9,
+			want: "2-",
+		},
+		{
+			name: "test 10",
+			sum:  10,
+			want: "20",
+		},
+		{
+			name: "test 15",
+			sum:  15,
+			want: "1=0",
+		},
+		{
+			name: "test 20",
+			sum:  20,
+			want: "1-0",
+		},
+		{
+			name: "test 2022",
+			sum:  2022,
+			want: "1=11-2",
+		},
+		{
+			name: "test 12345",
+			sum:  12345,
+			want: "1-0---0",
+		},
+		{
+			name: "test 314159265",
+			sum:  314159265,
+			want: "1121-1110-1=0",
+		},
+	}
+	for _, c := range tc {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			got := day_25.ConvertToSnafu(c.sum)
+			assert.Equal(t, c.want, got)
+		})
+	}
+}
