eeb4a15f0e3d457e26af396994f5e27318627b22
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-03-14 15:32:11 +0100

    puzzle solution day 1 part 1

--- rust/target/debug/deps/day_2-3033fe7c0174eb51.d
+++
@@ -1,5 +0,0 @@
-/Users/andrey.kolybelnikov/GolandProjects/advent/rust/target/debug/deps/day_2-3033fe7c0174eb51: crates/day_2/src/main.rs
-
-/Users/andrey.kolybelnikov/GolandProjects/advent/rust/target/debug/deps/day_2-3033fe7c0174eb51.d: crates/day_2/src/main.rs
-
-crates/day_2/src/main.rs:
