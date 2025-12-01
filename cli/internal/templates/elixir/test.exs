fe746d53e6d5995224d61cb7f86e767bba446e5d
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2025-12-01 13:19:08 +0200

    Update Elixir test template to bootstrap with file inputs like day 1

--- cli/internal/templates/elixir/test.exs
+++ cli/internal/templates/elixir/test.exs
@@ -2,17 +2,30 @@ defmodule Day{{DAY}}Test do
   use ExUnit.Case
   doctest Day{{DAY}}
 
-  @sample_input ""
+  @sample_input File.read!("test/inputs/day{{DAY}}_sample.txt")
+  @input File.read!("inputs/day{{DAY}}.txt")
 
-  test "part 1" do
+  test "part 1 sample" do
     expected = 0
     actual = Day{{DAY}}.part1(@sample_input)
     assert actual == expected
   end
 
-  test "part 2" do
+  test "part 1" do
+    expected = 0
+    actual = Day{{DAY}}.part1(@input)
+    assert actual == expected
+  end
+
+  test "part 2 sample" do
     expected = 0
     actual = Day{{DAY}}.part2(@sample_input)
+    assert actual == expected
+  end
+
+  test "part 2" do
+    expected = 0
+    actual = Day{{DAY}}.part2(@input)
     assert actual == expected
   end
 end
