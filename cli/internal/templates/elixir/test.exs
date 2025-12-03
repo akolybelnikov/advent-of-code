8d455bbeaa6ac3397e82d27a1a7dfc52c653b509
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2025-12-03 13:11:25 +0200

    feat: implement Day 03 Part 1 and corresponding tests; adjust CLI to bootstrap tests

--- cli/internal/templates/elixir/test.exs
+++ cli/internal/templates/elixir/test.exs
@@ -5,27 +5,31 @@ defmodule Day{{DAY}}Test do
   @sample_input File.read!("test/inputs/day{{DAY}}_sample.txt")
   @input File.read!("inputs/day{{DAY}}.txt")
 
-  test "part 1 sample" do
-    expected = 0
-    actual = Day{{DAY}}.part1(@sample_input)
-    assert actual == expected
+  describe "part 1" do
+    test "sample input" do
+      expected = 0
+      actual = Day{{DAY}}.part1(@sample_input)
+      assert actual == expected
+    end
+
+    test "real input" do
+      expected = 0
+      actual = Day{{DAY}}.part1(@input)
+      assert actual == expected
+    end
   end
 
-  test "part 1" do
-    expected = 0
-    actual = Day{{DAY}}.part1(@input)
-    assert actual == expected
-  end
+  describe "part 2" do
+    test "sample input" do
+      expected = 0
+      actual = Day{{DAY}}.part2(@sample_input)
+      assert actual == expected
+    end
 
-  test "part 2 sample" do
-    expected = 0
-    actual = Day{{DAY}}.part2(@sample_input)
-    assert actual == expected
-  end
-
-  test "part 2" do
-    expected = 0
-    actual = Day{{DAY}}.part2(@input)
-    assert actual == expected
+    test "real input" do
+      expected = 0
+      actual = Day{{DAY}}.part2(@input)
+      assert actual == expected
+    end
   end
 end
